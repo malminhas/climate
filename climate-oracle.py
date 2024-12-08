f7167696b550890818efac7a5c8f543b9340db08
Mal Minhas <mal@malm.co.uk>
2024-12-08 19:04:35 +0000

    Updated climate-oracle with RAG fixes and extended CLI

--- climate-oracle.py
+++ climate-oracle.py
@@ -17,24 +17,23 @@
 #
 # TODO:
 # -----
-# 1. Work out how to retrieve total tokens and cost for non-OpenAI examples
 #
 # History:
 # -------
 # 26.11.24    v0.1    First cut based on accompanying notebook
 # 26.11.24    v0.2    Added Anthropic and Ollama LLMs plus CLI support
 # 29.11.24    v0.3    First drop into climate repository
-#
+# 07.12.24    v0.4    Added cost output and force to regenerate vectorstore
+# 08.12.24    v0.5    Added tokencost for pricing, referencesswitched to Ollama default
 
 import os
 import time
 import logging
 from logging import Logger
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import OpenAI
+from langchain_openai import OpenAIEmbeddings,ChatOpenAI, OpenAI
 from langchain_ollama.llms import OllamaLLM
-from langchain_anthropic import AnthropicLLM, ChatAnthropic
+from langchain_anthropic import ChatAnthropic
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -43,14 +42,19 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.callbacks import get_openai_callback
 from langchain_core.runnables import Runnable
 from langchain_core.language_models import LLM
-from typing import List, Dict
+from typing import List, Dict, Tuple
+import requests
+from requests.exceptions import ConnectionError
 
 PROGRAM = __file__.split("/")[-1]
-VERSION = "0.3"
-DATE = "29.11.24"
+VERSION = "0.5"
+DATE = "08.12.24"
 AUTHOR = "<mal@malm.co.uk>"
 
-logger = None
+VALID_MODELS = ['ollama', 'gpt-3.5', 'gpt-4', 'claude']
+IPCC_AR6_PDF = "https://www.ipcc.ch/report/ar6/syr/downloads/report/IPCC_AR6_SYR_LongerReport.pdf"
+OLLAMA_URL = "http://localhost:11434"
+
 system_prompt = """You are the Climate Assistant, a helpful AI assistant.
 Your task is to answer common questions on climate change.
 You will be given a question and relevant excerpts from the IPCC Climate Change Synthesis Report (2023).
@@ -85,107 +89,191 @@ def initLogger(verbose: bool) -> Logger:
         pypdf_logger.setLevel(logging.ERROR)
     return logger
 
-def getModel(model: str) -> LLM:
+def is_ollama_running(logger: Logger) -> bool:
+    ''' Check if Ollama is running locally '''
+    logger.info(f"::is_ollama_running()")
+    try:
+        response = requests.get(f'{OLLAMA_URL}/api/tags')
+        return response.status_code == 200
+    except ConnectionError:
+        return False
+
+def getModel(model_provider: str, logger: Logger) -> LLM:
     ''' Create and return LLM instance based on input string. '''
-    logger.info(f"::getModel({model})")
-    name = ''
-    if model == 'ollama':
-        # See: https://python.langchain.com/docs/integrations/providers/ollama/
-        # pip install -U langchain-ollama
-        name = 'llama3.2'
-        llm = OllamaLLM(model=name, temperature=0)
-    elif model == 'claude':
-        # https://python.langchain.com/docs/integrations/providers/anthropic/
+    logger.info(f"::getModel({model_provider})")
+    model_name = ''
+    if model_provider in ['gpt-3.5']:
+        # See: https://python.langchain.com/docs/integrations/providers/openai
+        # pip install -U langchain-openai
+        model_name = 'gpt-3.5-turbo-instruct'
+        llm = OpenAI(model=model_name, temperature=0)
+    elif model_provider in ['gpt-4']:
+        # See: https://python.langchain.com/docs/integrations/providers/o1
+        # pip install -U langchain-o1
+        model_name = 'gpt-4o'
+        llm = ChatOpenAI(model=model_name, temperature=0)
+    elif model_provider in ['claude']:
+        # See: https://python.langchain.com/docs/integrations/providers/anthropic/
         # pip install -U langchain-anthropic
         # Model to use is 'claude-3-5-sonnet-latest'
         #name = 'claude-3-5-sonnet-latest'
         #llm = ChatAnthropic(model=name, temperature=0)
-        name = 'claude-2.1'
-        llm = AnthropicLLM(model=name, temperature=0)
+        model_name = 'claude-3-5-sonnet-latest'
+        print(f"Using Anthropic model='{model_name}'")
+        llm = ChatAnthropic(model=model_name, temperature=0)
+    elif model_provider in ['ollama']:
+        # Default to Ollama if no model name passed in at CLI - check if it is running locally first
+        logger.info("Checking if Ollama is running...")
+        if not is_ollama_running(logger):
+            raise RuntimeError(f"Ollama is not found at {OLLAMA_URL}. Please start Ollama first.")
+        # See: https://python.langchain.com/docs/integrations/providers/ollama/
+        # pip install -U langchain-ollama
+        model_name = 'llama3.2'
+        llm = OllamaLLM(model=model_name, temperature=0)
     else:
-        # Default to OpenAI gpt-3.5
-        # See: https://python.langchain.com/docs/integrations/providers/openai
-        name = 'gpt-3.5-turbo-instruct'
-        llm = OpenAI(model=name, temperature=0)
-    return name, llm
+        raise ValueError(f"Invalid model provider: '{model_provider}'. Valid options are: {VALID_MODELS}")
+    
+    return model_name, llm
 
-def generateChain(pdfs: List, model: str) -> (str, Runnable):
+def generateChain(pdfs: List, model_provider: str, logger: Logger, force: bool = False) -> Tuple[str, Runnable]:
     ''' Create RAG chain. '''
-    logger.info(f"::generateChain({model})")
-    # Prepare vector store (FAISS) with IPPC report(s).  Store splits in vectorstore
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, add_start_index=True)
-    for i, pdf in enumerate(pdfs):
-        loader = PyPDFLoader(pdf)
-        if i == 0:
-            vectorstore = FAISS.from_documents(documents=loader.load_and_split(text_splitter), embedding=OpenAIEmbeddings())
-        else:
-            vectorstore_i = FAISS.from_documents(documents=loader.load_and_split(text_splitter), embedding=OpenAIEmbeddings())
-            vectorstore.merge_from(vectorstore_i)
-    vectorstore.save_local('.')
-    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": len(pdfs)})
-    name, llm = getModel(model)
+    logger.info(f"::generateChain({model_provider})")
+    index_file = 'faiss_index'
+    if not force and os.path.exists(index_file):
+        print(f"Loading vectorstore from '{index_file}'")
+        vectorstore = FAISS.load_local(index_file, OpenAIEmbeddings(), allow_dangerous_deserialization=True)
+    else:
+        print(f"Preparing and saving vectorstore to '{index_file}'")
+        # Prepare vector store (FAISS) with IPPC report(s).  Store splits in vectorstore
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, add_start_index=True)
+        for i, pdf in enumerate(pdfs):
+            loader = PyPDFLoader(pdf)
+            if i == 0:
+                vectorstore = FAISS.from_documents(documents=loader.load_and_split(text_splitter), embedding=OpenAIEmbeddings())
+            else:
+                vectorstore_i = FAISS.from_documents(documents=loader.load_and_split(text_splitter), embedding=OpenAIEmbeddings())
+                vectorstore.merge_from(vectorstore_i)
+        vectorstore.save_local(index_file)
+        
+    # Original recipe had number of chunks as len(pdfs) but this is too few for a single document
+    #retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": len(pdfs)})
+    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 30})
+    model_name, llm = getModel(model_provider, logger)
         
     # Question-answering against an index using create_retrieval_chain:    
     prompt = PromptTemplate(template=system_prompt, input_variables=["question", "context"])
     
     document_chain = create_stuff_documents_chain(llm, prompt)
     qa_chain = create_retrieval_chain(retriever=retriever, combine_docs_chain=document_chain)
-    return name, qa_chain
+    return model_name, qa_chain
 
-def getAnswer(name: str, qa_chain: Runnable, question: str) -> Dict:
+def getAnswer(model_name: str, qa_chain: Runnable, question: str, logger: Logger) -> Dict:
     ''' Get answer. '''
-    logger.info(f"::getAnswer({name}, Q:'{question}')")
+    logger.info(f"::getAnswer({model_name}, Q:'{question}')")
     r = {}
-    if name in ['gpt-3.5-turbo-instruct']: 
-        with get_openai_callback() as cb:
-            answer = qa_chain.invoke({"input": system_prompt,"question": question})
-            logger.info(f"{answer}")
-            logger.info(f"{cb}")
-            r['answer'] = answer.get('answer').strip()
-            references = answer.get('context')
-            r['references'] = []
-            for i,reference in enumerate(references):
-                d = reference.to_json()
-                source = d.get('kwargs').get('metadata').get('source')
-                page = d.get('kwargs').get('metadata').get('page')
-                contents = d.get('kwargs').get('page_content')
-                r['references'].append(f"{source},{page}")
-            r['cost'] = cb.total_cost
-            r['prompt_tokens'] = cb.prompt_tokens
-            r['completion_tokens'] = cb.completion_tokens
-            r['total_tokens'] = cb.total_tokens
-    else:
-        answer = qa_chain.invoke({"input": system_prompt,"question": question})
-        logger.info(f"{answer}")
-        r['answer'] = answer.get('answer').strip()
+
+    # 1. Get the answer from the RAG results
+    answer = qa_chain.invoke({"input": system_prompt,"question": question})
+    r['answer'] = answer.get('answer').strip()
+    answer_text = r['answer']
+
+    # 2. Get the full context from the RAG results
+    context = answer.get('context', [])
+    context_text = "\n".join([doc.page_content for doc in context]) if context else ""
+    # Construct the full prompt including RAG content
+    full_prompt = system_prompt.format(context=context_text, question=question)
+    logger.info(f"Full prompt with RAG content:\n{full_prompt}")
+    # Add RAG references to return object
+    r['references'] = []
+    r['context'] = []
+    for i,doc in enumerate(context):
+        ref = doc.to_json()
+        source = f"source: {ref.get('kwargs').get('metadata').get('source')}"
+        page = f"page: {ref.get('kwargs').get('metadata').get('page')}"
+        chunk = ref.get('kwargs').get('page_content')
+        chunklen = len(chunk)
+        contents = f'chunk length: {chunklen}, snippet: "{chunk}"'
+        r['references'].append(f"{source}, {page}")
+        r['context'].append(contents)
+    # Derive counts using full context prompt
+    r['prompt_tokens'] = len(full_prompt.split())
+    r['completion_tokens'] = len(answer_text.split())
+    r['total_tokens'] = r['prompt_tokens'] + r['completion_tokens']
+
+    # 3. Get price of running the query
+    # Prices obtained from looking at pricing pages for each model per million tokens in USD:
+    # https://www.anthropic.com/pricing#anthropic-api
+    # https://platform.openai.com/pricing
+    mapping = {'llama3.2': [0.0,0.0],
+               'gpt-3.5-turbo-instruct': [1.5, 2.00],
+               'gpt-4o': [2.5, 10.00],
+               'claude-3-5-sonnet-latest': [3.0, 15.00]}
+    pc, cc = mapping[model_name] or [0.0, 0.0]
+    logger.info(f"Calculating [prompt,completion] costs for model='{model_name}' which maps to {pc,cc}")
+    print(f"Calculating [prompt,completion] costs for model='{model_name}' which maps to {pc,cc}")
+    prompt_cost = r['prompt_tokens'] * pc/1000000
+    completion_cost = r['completion_tokens'] * cc/1000000
+    r['cost'] = prompt_cost + completion_cost
     return r
-    
+
+def formatCosts(answer: Dict, logger: Logger) -> str:
+    ''' Format costs. '''
+    logger.info(f"::formatCosts()")
+    dollar_cost = answer.get('cost')
+    ptokens = answer.get('prompt_tokens')
+    ctokens = answer.get('completion_tokens')
+    ttokens = answer.get('total_tokens')
+    amount = round(dollar_cost, 4)
+    s = "---------- COST ----------\n"
+    s += f'cost=${amount}, prompt_tokens={ptokens}, completion_tokens={ctokens}, total_tokens={ttokens}'
+    return s
+
+def formatReferences(answer: Dict, logger: Logger) -> str:
+    ''' Format references. '''
+    snippet_length = 132
+    s = "\n---------- REFERENCES ----------\n"
+    for i,reference in enumerate(answer.get('references')):
+        chunk = answer.get('context')[i]
+        s += f" ======== RAG REFERENCE {i+1} ========\n{reference}\n{chunk[:snippet_length]}"
+        if len(chunk) > snippet_length:
+            s += "...\n"
+    return s
+
 def main(arguments: Dict):
     verbose = False
     cost = False
-    global logger
+    force = False
+    references = False
     logger = initLogger(False)
-    model = "openai"
+    model_provider = "ollama"
     if arguments.get("--verbose"):
         verbose = True
         logger = initLogger(verbose)
         logger.info(f"::main() - arguments =\n{arguments}")
     if arguments.get("--model"):
-        model = str(arguments.get("--model")[0])
+        model_provider = str(arguments.get("--model")[0])
     if arguments.get("--cost"):
         cost = True
-        
+    if arguments.get("--force"):
+        force = True
+    if arguments.get("--references"):
+        references = True
+
     if arguments.get("--version") or arguments.get("-V"):
         print(f"{PROGRAM} version {VERSION}.  Released {DATE} by {AUTHOR}")
     elif arguments.get("--help") or arguments.get("-h"):
         print(usage)
     else:
         t0 = time.time()
-        pdfs = ["https://www.ipcc.ch/report/ar6/syr/downloads/report/IPCC_AR6_SYR_LongerReport.pdf"]
-        print(f"Generating create_retrieval_chain using llm='{model}'...") 
-        name, climate_qa_chain = generateChain(pdfs, model)
+        pdfs = [IPCC_AR6_PDF]
+        print(f"Attempting to create_retrieval_chain using llm='{model_provider}'...")
+        try:
+            model_name, climate_qa_chain = generateChain(pdfs, model_provider, logger, force)
+        except Exception as e:
+            print(f"Error generating create_retrieval_chain: {e}")
+            return
         t1 = time.time()
-        print(f"successfully built RAG in {round((t1-t0),2)} seconds and will use '{name}' LLM")
+        print(f"successfully loaded RAG in {round((t1-t0),2)} seconds and will use '{model_name}' LLM")
         print("Enter a climate question (or press Enter to quit). eg. 'Is sea level rise avoidable and when will it stop?'")
         while True:
             try:
@@ -195,18 +283,17 @@ def main(arguments: Dict):
                 if question == "":
                     print("Empty input.  Exiting the program. Goodbye!")
                     break
-                answer = getAnswer(name, climate_qa_chain, question)
+                answer = getAnswer(model_name, climate_qa_chain, question, logger)
                 print(f"{answer.get('answer')}")
+                s = ''
                 if cost:
-                    cost = answer.get('cost')
-                    prompt_tokens = answer.get('prompt_tokens')
-                    completion_tokens = answer.get('completion_tokens')
-                    co2 = '???'
-                    print(f"cost={cost}, prompt_tokens={prompt_tokens}, completion_tokens={completion_tokens}, CO2={co2}")
+                    s += formatCosts(answer, logger)
+                if references and answer.get('references'):
+                    s += formatReferences(answer, logger)
+                print(s)
             except:                
                 print("Interrupt.  Exiting the program. Goodbye!")
                 break
-
 
 if __name__ == "__main__":
     import docopt
@@ -215,22 +302,28 @@ if __name__ == "__main__":
     {}
     ------------------
     Usage:
-    {} [-m <model>] [-c] [-v]
+    {} [-m <model>] [-c] [-r] [-f] [-v]
     {} -h | --help
     {} -V | --version
     Options:
     -h, --help                          Show this screen.
     -v, --verbose                       Verbose mode.
     -V, --version                       Show version.
-    -m <model>, --model <model>         LLM model.
-    -c, --cost                          Return cost of call.
+    -m <model>, --model <model>         LLM. Default is ollama. Options: gpt-3.5, gpt-4, claude.
+    -c, --cost                          Return cost of call in dollars + tokens
+    -r, --references                    Return RAG references
+    -f, --force                         Force regeneration of vectorstore.
     Examples:
-    1. Create verbose climate oracle using Anthropic Claude LLM:
+    1. Create verbose climate oracle using Anthropic claude-sonnet LLM:
     {} -m claude -v
-    2. Create climate oracle with cost output using Ollama + Llama3.2 LLM:
-    {} -m ollama -c
+    2. Create climate oracle with cost output using OpenAI gpt-3.5-turbo-instruct LLM:
+    {} -m gpt-3.5 -c
+    3. Create climate oracle using default Ollama and force regeneration of vectorstore:
+    {} -c -f
+    4. Create climate oracle with references using gpt-4o LLM:
+    {} -m gpt-4 -c -r
     """.format(
-        *tuple([PROGRAM] * 6)
+        *tuple([PROGRAM] * 8)
     )
     arguments = docopt.docopt(usage)
     main(arguments)
